a7975cf069ab8f33714e0e8581fa7ae00b0e0b3a
kleinay <ayal.s.klein@gmail.com>
2019-01-08 11:40:46 +0200

    QANom First commit

---
+++ catvar.py
@@ -0,0 +1,27 @@
+catvar_corpus_path = "resources/catvar21/catvar21.signed"
+
+def get_catvar_pairs():
+    # catvar_pairs is a list of 16K tuples (nominalization, source-verb) for 13.8K nominalizations
+
+    catvar_clusters = []
+    with open(catvar_corpus_path, "r") as f:
+        for line in f.readlines():
+            cluster = [] # cluster of Words, each Word is a tuple (word-surface, pos, signature-num)
+            for word_info in line.strip().split("#"):
+                word_and_pos, signature = word_info.split("%")
+                word, pos = word_and_pos.split("_")
+                cluster.append((word, pos, int(signature)))
+            catvar_clusters.append(cluster)
+    def get_pairs(c):
+        for noun in [w[0] for w in c if w[1]=='N']:
+            for verb in [w[0] for w in c if w[1]=='V']:
+                yield (noun, verb)
+    catvar_pairs = [list(get_pairs(c))
+                   for c in catvar_clusters
+                   if len(c) > 1]
+    flatten = lambda l: [item for sublist in l for item in sublist]
+    catvar_pairs = flatten(catvar_pairs)
+    return catvar_pairs
+catvar_pairs = get_catvar_pairs()
+def catvariate(noun):
+    return [verb for n,verb in catvar_pairs if n==noun]
---
+++ to_verb.py
@@ -0,0 +1,79 @@
+# (C) Gabriel Stanovsky
+from nltk.corpus import wordnet as wn
+
+# Just to make it a bit more readable
+WN_NOUN = 'n'
+WN_VERB = 'v'
+WN_ADJECTIVE = 'a'
+WN_ADJECTIVE_SATELLITE = 's'
+WN_ADVERB = 'r'
+
+def levenshteinDistance(s1, s2):
+    if len(s1) > len(s2):
+        s1, s2 = s2, s1
+
+    distances = range(len(s1) + 1)
+    for i2, c2 in enumerate(s2):
+        distances_ = [i2+1]
+        for i1, c1 in enumerate(s1):
+            if c1 == c2:
+                distances_.append(distances[i1])
+            else:
+                distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
+        distances = distances_
+    return distances[-1]
+
+
+
+def convert_pos(word, from_pos, to_pos):
+    """ Transform words given from/to POS tags """
+
+    synsets = wn.synsets(word, pos=from_pos)
+
+    # Word not found
+    if not synsets:
+        return []
+
+    # Get all lemmas of the word (consider 'a'and 's' equivalent)
+    lemmas = []
+    for s in synsets:
+        for l in s.lemmas():
+            if s.name().split('.')[1] == from_pos or from_pos in (WN_ADJECTIVE, WN_ADJECTIVE_SATELLITE) and s.name().split('.')[1] in (WN_ADJECTIVE, WN_ADJECTIVE_SATELLITE):
+                lemmas += [l]
+
+    # Get related forms
+    derivationally_related_forms = [(l, l.derivationally_related_forms()) for l in lemmas]
+
+    # filter only the desired pos (consider 'a' and 's' equivalent)
+    related_noun_lemmas = []
+
+    for drf in derivationally_related_forms:
+        for l in drf[1]:
+            if l.synset().name().split('.')[1] == to_pos or \
+                (to_pos in (WN_ADJECTIVE, WN_ADJECTIVE_SATELLITE)
+                 and l.synset().name().split('.')[1] in (WN_ADJECTIVE, WN_ADJECTIVE_SATELLITE)):
+                related_noun_lemmas += [l]
+
+    # Extract the words from the lemmas
+    related_words = [l.name() for l in related_noun_lemmas]
+    return list(set(related_words))
+
+
+def results_by_edit_distance(orig_word, optional_results):
+    # Build the result in the form of a list containing tuples (word, probability)
+    result = [(w, levenshteinDistance(w, orig_word)) for w in set(optional_results)]
+    result.sort(key=lambda w:len(w[0])) # tie breaker - prefer shorter verbs
+    result.sort(key=lambda w:-w[1], reverse = True)
+
+    # return all the possibilities sorted by probability
+    return result
+
+
+def verbalize(word, from_pos=WN_NOUN):
+    related_verbs = convert_pos(word, from_pos, WN_VERB)
+    return results_by_edit_distance(word, related_verbs)
+
+
+if __name__ == "__main__":
+    # example usage
+    print(convert_pos("recognition", WN_NOUN, WN_VERB))
