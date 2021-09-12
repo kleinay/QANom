daa8525ef50440a41bc2bfb8b4d7e622b598fac8
kleinay <ayal.s.klein@gmail.com>
2021-09-12 11:06:58 +0300

    pypi version 0.0.2; fixed setup's install_requires

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="qanom-kleinay",
-    version="0.0.1",
+    name="qanom",
+    version="0.0.2",
     author="Ayal Klein",
     author_email="ayal.s.klein@gmail.com",
     description="package for Question-Answer driven Semantic Role Labeling for Nominalizations (QANom)",
@@ -13,7 +13,14 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/kleinay/QANom",
     packages=setuptools.find_packages(),
-    install_requires=['git+https://github.com/NicolasBizzozzero/pattern@master'],
+    install_requires=[
+        'nltk',
+        'pandas',
+        'tqdm',
+        'sklearn',
+        'transformers==2.11.0',
+        'torch==1.4'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
