f4899e33941fdf862b8723ed553a1dd834829e05
kleinay <ayal.s.klein@gmail.com>
2022-10-02 12:55:06 +0300

    fix qanom.evaluate script - 'sentences' is now optional

--- qanom/evaluate.py
+++ qanom/evaluate.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     ap = ArgumentParser()
     ap.add_argument("system_path", help="system predicted annotations (CSV file)")
     ap.add_argument("ground_truth_path", help="reference annotations (CSV file")
-    ap.add_argument("sentences_path", default=None,
+    ap.add_argument("-s", "--sentences", default=None, required=False,
                     help="if the input files don't have a `sentence` column with the raw sentences (but only `qasrl_id`), provide a CSV file that maps `qasrl_id` to `sentence`.")
     args = ap.parse_args()
 
     from qanom.evaluation import evaluate
-    evaluate.main(args.system_path, args.ground_truth_path, args.sentences_path)
+    evaluate.main(args.system_path, args.ground_truth_path, args.sentences)
